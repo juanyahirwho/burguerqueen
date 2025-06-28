a0e5a980be8101b5153e147b7a46b12459027bef
juanyahirwho <juanyahirspz@gmail.com>
2025-06-28 15:23:18 -0600

    App lista, aunque faltan destalles para el despliegue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   urlApi: 'https://90d7-38-49-148-38.ngrok-free.app/api/v1/',
-  publishKey: 'pk_test_51Rf3gwRmGKSbHPdN3B9ztGdXyUukt4AKJVC0Cchk7TJOq4kJPevyawfhHAnI3hRiSbp0WokwrtiJqHSgzmmOs7Dd00wek0uVYB',
-  secretKey: 'sk_test_51Rf3gwRmGKSbHPdNhiarqoA73RNsoYPhXyF4lygHWGUPpybkf2CtDetR1I1FUBmG7AwTGs2AMBJXIAdzi8472QtV00JzNkN47H'
+  publishKey: '',
+  secretKey: ''
 };
